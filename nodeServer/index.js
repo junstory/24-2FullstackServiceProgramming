d0a7822f4146b0cfb02cfc7bf0293b5f85fb2eab
junstory <lastcom1@gmail.com>
2024-10-27 19:24:42 +0900

    eslint setting

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req,res) => {
-    res.json({
-        success: true,
-    });
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+  });
 });
 
-app.listen(port, ()=>{
-    console.log(`server is listening at ${port}`);
+app.listen(port, () => {
+  console.log(`server is listening at ${port}`);
 });
