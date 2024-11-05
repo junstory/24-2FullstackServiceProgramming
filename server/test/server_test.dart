32778e2946f10b29549f0bb42d41adff74c8e4c1
junstory <lastcom1@gmail.com>
2024-11-05 14:36:28 +0900

    Create. Server테스트 코드 추가, 수정

--- server/test/server_test.dart
+++ server/test/server_test.dart
@@ -3,37 +3,42 @@ import 'package:dio/dio.dart';
 class UserService {
   final Dio _dio = Dio(BaseOptions(baseUrl: 'http://127.0.0.1:3000/api/v1')); // 기본 URL을 실제 API URL로 변경
 
-  Future<void> createUser(Map<String, dynamic> userData) async {
+  Future<int> createUser(Map<String, dynamic> userData) async {
     try {
-      final response = await _dio.post('/user/', data: userData);
-      print('User created: ${response.data}');
+      final response = await _dio.post<Map<String, dynamic>>('/user/', data: userData);
+      print('User created: ${response.data}\n');
+      //print('User created: ${response.data!['result']!['userId']}');
+      return response.data != null ? response.data!['result']!['userId'] as int : throw Exception('User ID not found');
     } catch (e) {
       print('Failed to create user: $e');
+      throw Exception('Failed to create user');
     }
   }
 
-  Future<void> getUser(String userId) async {
+  Future<void> getUser(int userId) async {
     try {
       final response = await _dio.get('/user/$userId');
-      print('User details: ${response.data}');
+      print('User details: ${response.data}\n');
     } catch (e) {
       print('Failed to get user: $e');
     }
   }
 
-  Future<void> deleteUser(String userId) async {
+  Future<void> deleteUser(int userId) async {
     try {
       final response = await _dio.delete('/user/$userId');
-      print('User deleted: ${response.statusCode}');
+      print('User deleted: ${response.data}\n');
     } catch (e) {
       print('Failed to delete user: $e');
     }
   }
 
-  Future<void> updateUser(String userId, Map<String, dynamic> updatedData) async {
+  Future<void> updateUser(int userId, Map<String, dynamic> updatedData) async {
     try {
       final response = await _dio.put('/user/$userId', data: updatedData);
       print('User updated: ${response.data}');
+      final userService = UserService();
+      await userService.getUser(userId);
     } catch (e) {
       print('Failed to update user: $e');
     }
@@ -42,22 +47,23 @@ class UserService {
 
 void main() async {
   final userService = UserService();
+  int userId;
 
   // Create a new user
-  await userService.createUser({
+  userId = await userService.createUser({
     'name': 'John Doe',
     'email': 'johndoe@example.com',
   });
 
   // Get a user by ID
-  await userService.getUser('30');
+  await userService.getUser(userId);
 
   // Update user information
-  await userService.updateUser('123', {
-    'name': 'Jane Doe',
-    'email': 'janedoe@example.com',
+  await userService.updateUser(userId, {
+    'name': 'Kane Doe',
+    'email': 'kanedoe@example.co.kr',
   });
 
   // Delete a user by ID
-  await userService.deleteUser('123');
+  await userService.deleteUser(userId);
 }
