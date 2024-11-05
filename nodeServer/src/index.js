75758856c5ce64890a747c90fe349f9aff328be8
junstory <lastcom1@gmail.com>
2024-11-05 13:18:33 +0900

    Update. 들어오는 입력 확인

--- nodeServer/src/index.js
+++ nodeServer/src/index.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 //app.use(cors());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/commute', commuteRouter);
 app.use('/api/v1/schedule', scheduleRouter);
