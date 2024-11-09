5ead7a9bc6ec945c149a8034c3202286ae0915bc
junstory <lastcom1@gmail.com>
2024-11-09 17:06:40 +0900

    Feat. company CRUD 구현

--- nodeServer/src/routes/company.route.js
+++ nodeServer/src/routes/company.route.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import {
+  companyCreateController,
+  companyGetInfoController,
+  companyUpdateController,
+  companyDeleteController,
+} from '../controllers/company.controller.js';
 
 export const companyRouter = express.Router({ mergeParams: true });
 
-// commuteRouter.get('/:userId', userGetInfoController)
-// commuteRouter.post('/:userId', userEditProfileController)
-
+companyRouter.post('/', companyCreateController);
+companyRouter.get('/:companyId', companyGetInfoController);
+companyRouter.put('/:companyId', companyUpdateController);
+companyRouter.delete('/:companyId', companyDeleteController);
 export default companyRouter;
