3b7ee4a2d71831b295b486aff2fa4a3e283b7325
junstory <lastcom1@gmail.com>
2024-12-12 22:31:11 +0900

    DELETE. 사용하지 않은 폴더 정리

--- server/routes/index.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-
-Response onRequest(RequestContext context) {
-  final request = context.request;
-
-  final params = request.uri.queryParameters;
-  return Response(body: 'Welcome to Dart Frog!$params');
-}
